091b57f47e629d7ae273dc948e0a5a671817eed7
Menarin <39098584+Menarin@users.noreply.github.com>
2018-06-08 14:12:35 -0500

    LSP solucion

---
+++ tallerSOLID/src/LSPsolution/Radio.java
@@ -0,0 +1,14 @@
+package LSPviolation;
+
+public class Radio {
+	private boolean encendida;
+	
+	public boolean isEncendida() {
+		return encendida;
+	}
+	public void encender(){
+
+		this.encendida = true;
+
+	}
+}
---
+++ tallerSOLID/src/LSPsolution/testeadorDeAdicionales.java
@@ -0,0 +1,8 @@
+package LSPviolation;
+
+import vehiculos.Vehiculo;
+
+public class testeadorDeAdicionales {
+	
+	public void testearAdicionalRadio(Vehiculo vehiculo) {vehiculo.escuchar();	}
+}
