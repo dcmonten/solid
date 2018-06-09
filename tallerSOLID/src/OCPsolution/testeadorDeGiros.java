d29183109dddd53df6d5bb350b3471a0e3fd715e
AxleJouberth317 <jr_gambetta@hotmail.com>
2018-06-08 20:04:19 -0500

    Integrante 1

---
+++ tallerSOLID/src/OCPsolution/testeadorDeGiros.java
@@ -0,0 +1,16 @@
+package OCPsolution;
+
+import vehiculos.Bicicleta;
+import vehiculos.Carro;
+
+public class testeadorDeGiros {
+        
+        public void girarLlantasIzquierda(VehiculoConRuedas vRuedas){
+            
+        }
+        
+        public void girarLlantasDerecha(VehiculoConRuedas vRuedas){
+            
+        }
+        
+}
