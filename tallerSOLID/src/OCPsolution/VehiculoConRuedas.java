92556d489c19cdc9c0e0c86ab25cdec310ae814e
AxleJouberth317 <jr_gambetta@hotmail.com>
2018-06-08 20:07:29 -0500

    Solución OCP

---
+++ tallerSOLID/src/OCPsolution/VehiculoConRuedas.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package OCPsolution;
+
+import vehiculos.Vehiculo;
+
+/**
+ *
+ * @author Jouberth
+ */
+public abstract class VehiculoConRuedas {
+    
+    public abstract void girarLlantasIzquierda();
+    
+    public abstract void girarLlantasDerecha();
+        
+}
